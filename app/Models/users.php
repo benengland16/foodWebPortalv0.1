1438eac6b47dd10417b32cee53bfccec371b2e54
benengland16 <52063570+benengland16@users.noreply.github.com>
2020-08-26 13:44:32 +1000

    Update users.php

--- app/Models/users.php
+++ app/Models/users.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class users extends Model
 {
     
-	protected $table = 'users';
+	protected $fillable = [
+        'name', 'email', 'password', 'role_id', 'company_id', 'created_by', 'updated_by', 'deleted_by'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
 }
