bb5d557da3a27ab361d45869c6ae4080920940fe
benengland16 <52063570+benengland16@users.noreply.github.com>
2021-04-14 19:10:11 +1000

    trimmed

--- resources/views/routeNames/admin/routes.blade.php
+++
@@ -1,1 +0,0 @@
-<?php include(base_path()."/resources/views/routeNames/routesCommon.blade.php"); ?>
--- routes/web.php
+++ routes/web.php
@@ -27,59 +27,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 //Route::get('/dashboard','DashboardController@index')->name('superAdmin.dashboard.index');
 
-        Route::middleware('SessionHasSuperAdmin')->group(function () {
+        
 
 
-            Route::group(['prefix' => 'super-admin', 'namespace' => 'SuperAdmin'], function() {
-
-
-                Route::get('/dash-board','DashboardController@index')->name("superAdmin.dashboard.index");
-
-                Route::get('/user-index','UserController@index')->name("superAdmin.user.index");
-                Route::get('/user-create','UserController@create')->name("superAdmin.user.create");
-                Route::post('/user-store','UserController@store')->name("superAdmin.user.store");
-                Route::delete('/user-delete/{id}','UserController@delete')->name("superAdmin.user.delete");
-
-                Route::get('/recipe-index','RecipeController@index')->name("superAdmin.recipe.index");
-
-                Route::get('/company-index','CompanyController@index')->name("superAdmin.company.index");
-
-                Route::get('/company-recipe-index/{id}','CompanyRecipeController@index')->name("superAdmin.companyRecipe.index");
-                Route::delete('/company-recipe-delete/{id}','CompanyRecipeController@delete')->name("superAdmin.companyRecipe.delete");
-
-
-
-            });
-
-            Route::group(['prefix' => 'super-admin', 'namespace' => 'Common'], function() {
-                $role="superAdmin";
-                include base_path().'/routes/common_routes.php';
-            });
-
-
-
-        });
-
-
-        Route::middleware('SessionHasAdmin')->group(function () {
-           
-
-            Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
-                
-                $role="admin";
-                Route::get('/dash-board','DashboardController@index')->name("$role.dashboard.index");
-                
-
-                
-            });
-
-            Route::group(['prefix' => 'admin', 'namespace' => 'Common'], function() {
-                $role="admin";
-                include base_path().'/routes/common_routes.php';
-            });
-
-                
-        });
+        
 
 
 
@@ -107,34 +58,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 
-        Route::middleware('SessionHasDistributor')->group(function () {
-           
-            Route::group(['prefix' => 'distributor', 'namespace' => 'Distributor'], function() {
-                $role="distributor";
-                Route::get('/dash-board','DashboardController@index')->name("$role.dashboard.index");
-            });
-
-            Route::group(['prefix' => 'distributor', 'namespace' => 'Common'], function() {
-                $role="distributor";
-                include base_path().'/routes/common_routes.php';
-            });
-
-                
-        });
+        
 
 
 
-        Route::middleware('SessionHasManufacturer')->group(function () {
-           
-            Route::group(['prefix' => 'manufacturer', 'namespace' => 'Manufacturer'], function() {
-                $role="manufacturer";
-                Route::get('/dash-board','DashboardController@index')->name("$role.dashboard.index");
-            });
-
-            Route::group(['prefix' => 'manufacturer', 'namespace' => 'Common'], function() {
-                $role="manufacturer";
-                include base_path().'/routes/common_routes.php';
-            });
-
-                
-        });
+        
